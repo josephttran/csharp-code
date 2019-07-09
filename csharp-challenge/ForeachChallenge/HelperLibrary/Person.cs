3c4601306332c17674dcd8fa58b1440453b6c4ee
josephttran <42635012+josephttran@users.noreply.github.com>
2019-07-08 22:45:53 -0700

    Initial commit

---
+++ csharp-challenge/ForeachChallenge/HelperLibrary/Person.cs
@@ -0,0 +1,8 @@
+namespace HelperLibrary
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ csharp-challenge/ForeachChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HelperLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static List<Person> people = new List<Person>();
+
+        static void Main(string[] args)
+        {
+            SetUpData();
+
+            GreetAllPeople();
+        }
+        
+        private static void SetUpData()
+        {
+            people.Add(new Person { Firstname = "Alice", Lastname = "Eve" });
+            people.Add(new Person { Firstname = "Bob", Lastname = "Tom" });
+            people.Add(new Person { Firstname = "Carry", Lastname = "Sandy" });
+        }
+
+        private static void GreetAllPeople()
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Hello { person.Firstname } { person.Lastname }");                
+            }
+        }
+    }
+}
