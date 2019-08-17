7eb11700a2a37cededd160091ba15a2a43a84950
josephttran <42635012+josephttran@users.noreply.github.com>
2019-08-17 01:59:37 -0700

    Add solution for error handling bonus challenge

--- csharp-challenge/ErrorHandlingChallenge/ConsoleUI/Program.cs
+++ csharp-challenge/ErrorHandlingChallenge/ConsoleUI/Program.cs
@@ -9,9 +9,30 @@ namespace ConsoleUI
 {
     class Program
     {
+        static void HandleException(Exception ex, string message)
+        {
+            Exception inner = ex.InnerException;
+
+            if (inner == null)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.Write($"{ message } ");
+
+                while (inner != null)
+                {
+                    Console.WriteLine(inner.Message);
+                    inner = inner.InnerException;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             PaymentProcessor paymentProcessor = new PaymentProcessor();
+
             for (int i = 0; i <= 10; i++)
             {
                 try
@@ -27,11 +48,20 @@ namespace ConsoleUI
                         Console.WriteLine(result.TransactionAmount);
                     }
                 }
-                catch (Exception)
+                catch (IndexOutOfRangeException ex)
                 {
-                    Console.WriteLine($"Payment skipped for payment with { i } items");
+                    HandleException(ex, "Skipped invalid record");
+                }
+                catch (FormatException ex) when (i != 5)
+                {
+                    HandleException(ex, "Formatting Issue");
+                }
+                catch (Exception ex)
+                {
+                    HandleException(ex, $"Payment skipped for payment with { i } items");
                 }
             }
+
             Console.ReadLine();
         }
     }
