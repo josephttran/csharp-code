1c610bc6020782f895920416a7a7afbf7d814947
josephttran <42635012+josephttran@users.noreply.github.com>
2019-08-28 22:37:50 -0700

    Read a connection string and a app setting from config file

--- csharp-challenge/ConfigFileChallenge/ConsoleUI/Program.cs
+++ csharp-challenge/ConfigFileChallenge/ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ReadConnectionString("Default");
+            ReadAppSetting("UserName");
+
+            Console.ReadLine();
+        }
+
+
+        static void ReadConnectionString(string name)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            Console.WriteLine($"Connection string for { name }: { connectionString }");
+        }
+
+        static void ReadAppSetting(string key)
+        {
+            string appSetting = ConfigurationManager.AppSettings[key];
+            Console.WriteLine($"App setting value for { key }: { appSetting }");
         }
     }
 }
