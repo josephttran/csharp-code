2d2699fa4c519651c3ea3b9cb14dd7b1e9bb2670
josephttran <42635012+josephttran@users.noreply.github.com>
2019-09-16 01:58:41 -0700

    Refactor to reduce modulo operation

--- csharp-challenge/FizzBuzzChallenge/ConsoleUI/Program.cs
+++ csharp-challenge/FizzBuzzChallenge/ConsoleUI/Program.cs
@@ -6,23 +6,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            string word;
+
             for (int number = 1; number <= 100; number++)
             {
-                if (number % 3 == 0 && number % 5 == 0)
+                word = "";
+
+                if (number % 3 == 0)
                 {
-                    Console.WriteLine($"FizzBuzz ({ number })");
+                    word = "Fizz";
                 }
-                else if (number % 3 == 0)
+
+                if (number % 5 == 0)
                 {
-                    Console.WriteLine($"Fizz ({ number })");
-                }                
-                else if (number % 5 == 0)
-                { 
-                    Console.WriteLine($"Buzz ({ number })");
+                    word += "Buzz";
+                }
+
+                if (word == "")
+                {
+                    Console.WriteLine(number);
                 }
                 else
                 {
-                    Console.WriteLine(number);
+                    Console.WriteLine($"{ word } ({ number })");
                 }
             }
         }
