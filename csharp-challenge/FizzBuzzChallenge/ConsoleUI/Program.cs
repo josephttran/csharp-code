3012961f29109f239bcc7b7357d1d680d0e8cce7
josephttran <42635012+josephttran@users.noreply.github.com>
2019-09-16 01:38:38 -0700

    Add solution to fizz buzz challenge

---
+++ csharp-challenge/FizzBuzzChallenge/ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine($"FizzBuzz ({ number })");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine($"Fizz ({ number })");
+                }                
+                else if (number % 5 == 0)
+                { 
+                    Console.WriteLine($"Buzz ({ number })");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
