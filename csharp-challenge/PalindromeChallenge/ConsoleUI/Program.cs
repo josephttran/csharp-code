bcfebed51ca7b7d0672a2180cc224972609bd017
josephttran <42635012+josephttran@users.noreply.github.com>
2019-09-16 22:58:06 -0700

    Add overload method to handle double

--- csharp-challenge/PalindromeChallenge/ConsoleUI/Program.cs
+++ csharp-challenge/PalindromeChallenge/ConsoleUI/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleUI
         {
             string str = "ra c22.rea ER%&$22car";
             int number = 12321;
+            double myDouble = 12.321;
 
             if (IsPalindrome(str))
             {
@@ -26,6 +27,15 @@ namespace ConsoleUI
             else
             {
                 Console.WriteLine($"{ number } is not a palindrome");
+            }
+
+            if (IsPalindrome(myDouble))
+            {
+                Console.WriteLine($"{ myDouble } is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{ myDouble } is not a palindrome");
             }
         }
 
@@ -57,6 +67,23 @@ namespace ConsoleUI
         static public bool IsPalindrome(int number)
         {
             string str = number.ToString();
+
+            for (int index = 0; index < str.Length / 2; index++)
+            {
+                if (str[index] != str[str.Length - 1 - index])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static public bool IsPalindrome(double myDouble)
+        {
+            string str = myDouble.ToString().ToLower();
+
+            str = Regex.Replace(str, @".", String.Empty);
 
             for (int index = 0; index < str.Length / 2; index++)
             {
