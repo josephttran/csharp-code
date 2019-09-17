77632bfd9898fd462ddb0a013c5209ccd8bd2f2e
josephttran <42635012+josephttran@users.noreply.github.com>
2019-09-16 21:33:46 -0700

    Add solution to palindrome challenge

---
+++ csharp-challenge/PalindromeChallenge/ConsoleUI/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "racecar";
+            int number = 12321;
+
+            if (IsPalindrome(str))
+            {
+                Console.WriteLine($"{ str } is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{ str } is not a palindrome");
+            }
+
+            if (IsPalindrome(number))
+            {
+                Console.WriteLine($"{ number } is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{ number } is not a palindrome");
+            }
+        }
+
+        static public bool IsPalindrome(string str)
+        {
+            for (int index = 0; index < str.Length / 2; index++)
+            {
+                if (str[index] != str[str.Length - 1 - index])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static public bool IsPalindrome(int number)
+        {
+            string str = number.ToString();
+
+            for (int index = 0; index < str.Length / 2; index++)
+            {
+                if (str[index] != str[str.Length - 1 - index])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
