bd3d37cc71e075ae5787356d0be466cfcf6a9ef7
josephttran <42635012+josephttran@users.noreply.github.com>
2019-09-26 14:06:03 -0700

    Calculate change for correct change challenge

---
+++ csharp-challenge/CorrectChangeChallenge/ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string formatSpecifier = "C";
+            decimal amountOwe = 0.10m;
+            decimal amountPaid = 1.00m;
+            decimal changeAmount = CalculateChangeAmount(amountOwe, amountPaid);
+
+            PrintChangeInfo(formatSpecifier, amountOwe, amountPaid, changeAmount);
+        }
+
+        static decimal CalculateChangeAmount(decimal amountDue, decimal amountPaid)
+        {
+            return decimal.Subtract(amountPaid, amountDue);
+        }
+
+        static void PrintChangeInfo(string formatSpecifier, decimal amountOwe, decimal amountPaid, decimal change)
+        {
+            Console.WriteLine($"Amount to pay: { amountOwe.ToString(formatSpecifier) }");
+            Console.WriteLine($"Amount paid: { amountPaid.ToString(formatSpecifier) }");
+            Console.WriteLine($"The amount of change back is { change.ToString(formatSpecifier) }");
+        }
+    }
+}
