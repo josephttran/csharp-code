95da9794406a3cd3b39eee5f260f9b8692fd5fd9
josephttran <42635012+josephttran@users.noreply.github.com>
2019-09-26 15:12:11 -0700

    Get and display change in coins

--- csharp-challenge/CorrectChangeChallenge/ConsoleUI/Program.cs
+++ csharp-challenge/CorrectChangeChallenge/ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleUI
 {
@@ -6,12 +8,21 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Dictionary<string, decimal> coins = new Dictionary<string, decimal>()
+            {
+                { "quarter", 0.25m },
+                { "dime", 0.10m },
+                { "nickel", 0.05m },
+                { "penny", 0.01m }
+            };
+
             string formatSpecifier = "C";
             decimal amountOwe = 0.10m;
             decimal amountPaid = 1.00m;
             decimal changeAmount = CalculateChangeAmount(amountOwe, amountPaid);
+            Dictionary<string, int> change = GetChangeInCoin(coins, changeAmount);
 
-            PrintChangeInfo(formatSpecifier, amountOwe, amountPaid, changeAmount);
+            PrintChangeInfo(formatSpecifier, amountOwe, amountPaid, changeAmount, change);
         }
 
         static decimal CalculateChangeAmount(decimal amountDue, decimal amountPaid)
@@ -19,11 +30,37 @@ namespace ConsoleUI
             return decimal.Subtract(amountPaid, amountDue);
         }
 
-        static void PrintChangeInfo(string formatSpecifier, decimal amountOwe, decimal amountPaid, decimal change)
+        static Dictionary<string, int> GetChangeInCoin(Dictionary<string, decimal> coins, decimal changeAmount)
+        {
+            Dictionary<string, int> change = new Dictionary<string, int>();
+
+            foreach (KeyValuePair<string, decimal> coin in coins)
+            {
+                while (coin.Value <= changeAmount)
+                {
+                    if (!change.ContainsKey(coin.Key))
+                    {
+                        change.Add(coin.Key, 1);
+                    }
+                    else
+                    {
+                        change[coin.Key]++;
+                    }
+
+                    changeAmount = decimal.Subtract(changeAmount, coin.Value);
+                }
+            }
+
+            return change;
+        }
+
+        static void PrintChangeInfo(string formatSpecifier, decimal amountOwe, decimal amountPaid, decimal changeAmount, Dictionary<string, int> change)
         {
             Console.WriteLine($"Amount to pay: { amountOwe.ToString(formatSpecifier) }");
             Console.WriteLine($"Amount paid: { amountPaid.ToString(formatSpecifier) }");
-            Console.WriteLine($"The amount of change back is { change.ToString(formatSpecifier) }");
+            Console.WriteLine($"The amount of change back is { changeAmount.ToString(formatSpecifier) }");
+            Console.WriteLine($"\nThe following change to give back: ");
+            Console.WriteLine(String.Join(", ", change.Select(kv => $"{ kv.Value } { kv.Key }")));
         }
     }
 }
