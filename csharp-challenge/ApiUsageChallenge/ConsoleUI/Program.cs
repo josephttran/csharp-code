5a2b297242139ed37dcb5a114845166ed4270615
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-03 13:13:49 -0700

    Get user input

--- csharp-challenge/ApiUsageChallenge/ConsoleUI/Program.cs
+++ csharp-challenge/ApiUsageChallenge/ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 using APIHelperLibrary.StarWars.Model;
 using APIHelperLibrary.StarWars.Services;
@@ -10,10 +11,36 @@ namespace ConsoleUI
         static async Task Main(string[] args)
         {
             var starWarsPeopleServices = new StarWarsPeopleServices();
-            int personId = 2;
+            int personId = GetIdFromInput();
             StarWarsPerson starWarsPerson = await starWarsPeopleServices.GetStarWarsPerson(personId);
 
             starWarsPeopleServices.ShowPrettyJson(starWarsPerson);
+        }
+
+        static int GetIdFromInput()
+        {
+            int id = -1;
+            bool valid = false;
+
+            Console.Write("Enter an ID: ");
+            string userInput = Console.ReadLine();
+
+            while (!valid)
+            {
+                if (int.TryParse(userInput, out int inputID))
+                {
+                    valid = true;
+                    id = inputID;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Please enter a number!");
+                    Console.Write("\nEnter an ID: ");
+                    userInput = Console.ReadLine();
+                }
+            }
+
+            return id;
         }
     }
 }
