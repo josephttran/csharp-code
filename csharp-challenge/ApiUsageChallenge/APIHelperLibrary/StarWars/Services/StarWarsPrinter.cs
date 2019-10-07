ebf833686b1bc8d1ed9d556115349ed4267e568f
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-06 17:01:32 -0700

    Remove unused namespace

--- csharp-challenge/ApiUsageChallenge/APIHelperLibrary/StarWars/Services/StarWarsPrinter.cs
+++ csharp-challenge/ApiUsageChallenge/APIHelperLibrary/StarWars/Services/StarWarsPrinter.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
-using System.Text.RegularExpressions;
 
 namespace APIHelperLibrary.StarWars.Services
 {
