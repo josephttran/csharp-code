59685556088c2a180fe60a2e6d3c7b58f822ef75
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-08 22:20:49 -0700

    Add word count challenge starter code

---
+++ csharp-challenge/WordCountChallenge/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        private static string[] tests = new string[]
+        {
+            @"The test of the 
+            best way to handle
+
+multiple lines,   extra spaces and more.",
+            @"Using the starter app, create code that will 
+loop through the strings and identify the total 
+character count, the number of characters
+excluding whitespace (including line returns), and the
+number of words in the string. Finally, list each word, ensuring it
+is a valid word."
+        };
+
+        /* 
+            First string (tests[0]) Values:
+            Total Words: 14
+            Total Characters: 89
+            Character count (minus line returns and spaces): 60
+            Most used word: the (2 times)
+            Most used character: e (10 times)
+
+            Second string (tests[1]) Values:
+            Total Words: 45
+            Total Characters: 276
+            Character count (minus line returns and spaces): 230
+            Most used word: the (6 times)
+            Most used character: t (24 times)
+        */
+
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
