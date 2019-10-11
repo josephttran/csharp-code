99eb3d65d210db14708b33542461345b913a067f
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-11 00:18:33 -0700

    Add solution to advanced parameter challenge

--- csharp-challenge/AdvancedParameterChallenge/ConsoleUI/Program.cs
+++ csharp-challenge/AdvancedParameterChallenge/ConsoleUI/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleUI
 
             Console.WriteLine($"Using out parameter: { firstOutText }, { secondOutText }");
             Console.WriteLine($"Using tuple: { firstTupleText }, { secondTupleText }");
+
+            AnotherMethod();
         }
 
         static void UseOutParameterExample(out string firstOutText, out string secondOutText)
@@ -23,6 +25,27 @@ namespace ConsoleUI
         static (string firstText, string secondText) UseTupleExample()
         {
             return ("first item", "second item");
+        }
+
+        static void AnotherMethod()
+        {
+            string anotherText;
+
+            FirstModifyVariableWithoutReturn(out anotherText);
+            Console.WriteLine($"First way modified variable: { anotherText }");
+
+            SecondModifyVariableWithoutReturn(ref anotherText);
+            Console.WriteLine($"Second way modified variable: { anotherText }");
+        }
+
+        static void FirstModifyVariableWithoutReturn(out string text)
+        {
+            text = "modified variable with out parameter";
+        }
+
+        static void SecondModifyVariableWithoutReturn(ref string text)
+        {
+            text = "modified variable with ref parameter";
         }
     }
 }
