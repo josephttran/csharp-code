066b5b6d14fcbb5f583b40dabec3f1da7517866e
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-10 23:50:10 -0700

    Use out parameter and tuple

---
+++ csharp-challenge/AdvancedParameterChallenge/ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string firstTupleText, string secondTupleText) = UseTupleExample();
+
+            UseOutParameterExample(out string firstOutText, out string secondOutText);
+
+            Console.WriteLine($"Using out parameter: { firstOutText }, { secondOutText }");
+            Console.WriteLine($"Using tuple: { firstTupleText }, { secondTupleText }");
+        }
+
+        static void UseOutParameterExample(out string firstOutText, out string secondOutText)
+        {
+            firstOutText = "first out parameter text";
+            secondOutText = "second out parameter text";
+        }
+
+        static (string firstText, string secondText) UseTupleExample()
+        {
+            return ("first item", "second item");
+        }
+    }
+}
