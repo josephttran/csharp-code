9e48ed303622755db12a2bd4b8a2ce5526ece8d1
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-14 21:08:36 -0700

    Add first solution to copying objects

--- csharp-challenge/CopyingObjectsChallenge/CopyObjectsUI/Program.cs
+++ csharp-challenge/CopyingObjectsChallenge/CopyObjectsUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,16 +35,74 @@ namespace CopyObjectsUI
                 }
             };
 
+            string newName = "Bob";
+            string newAddress = "123 Bob Address";
+
             // Creates a second PersonModel object
-            PersonModel secondPerson = null;
+            PersonModel secondPerson = new PersonModel();
 
             // Set the value of the secondPerson to be a copy of the firstPerson
+            CopyPersonMethodOne(firstPerson, secondPerson);
 
             // Update the secondPerson's FirstName to "Bob" 
-            // and change the Street Address for each of Bob's addresses
-            // to a different value
+            secondPerson.FirstName = newName;
+
+            // Change the Street Address for each of Bob's addresses to a different value
+            SetPersonStreetAddress(secondPerson, newAddress);
 
             // Ensure that the following statements are true
+            ComparePerson(firstPerson, secondPerson);
+
+            Console.ReadKey();
+        }
+
+        static void CopyPersonMethodOne(PersonModel firstPerson, PersonModel secondPerson)
+        {
+            PropertyInfo[] firstPersonProperties = firstPerson.GetType().GetProperties();
+
+            foreach (var property in firstPersonProperties)
+            {
+                if (property.PropertyType.Name.ToString() == "List`1")
+                {
+                    if (property.GetValue(firstPerson) != null)
+                    {
+                        List<AddressModel> addresses = new List<AddressModel>();
+                        List<AddressModel> firstPersonAddresses = property.GetValue(firstPerson) as List<AddressModel>;
+
+                        foreach (AddressModel address in firstPersonAddresses)
+                        {
+                            AddressModel newAddress = new AddressModel
+                            {
+                                StreetAddress = address.StreetAddress,
+                                City = address.City,
+                                State = address.State,
+                                ZipCode = address.ZipCode
+                            };
+
+                            addresses.Add(newAddress);
+                        }
+
+                        secondPerson.GetType().GetProperty(property.Name).SetValue(secondPerson, addresses);
+                    }
+                }
+                else
+                {
+                    var value = property.GetValue(firstPerson);
+                    secondPerson.GetType().GetProperty(property.Name).SetValue(secondPerson, value);
+                }
+            }
+        }
+
+        static void SetPersonStreetAddress(PersonModel person, string newAddress)
+        {
+            foreach (AddressModel address in person.Addresses)
+            {
+                address.StreetAddress = newAddress;
+            }
+        }
+
+        static void ComparePerson(PersonModel firstPerson, PersonModel secondPerson)
+        {
             Console.WriteLine($"{ firstPerson.FirstName } != { secondPerson.FirstName }");
             Console.WriteLine($"{ firstPerson.LastName } == { secondPerson.LastName }");
             Console.WriteLine($"{ firstPerson.DateOfBirth.ToShortDateString() } == { secondPerson.DateOfBirth.ToShortDateString() }");
