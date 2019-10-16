5cc0011a5245ec515b378d7a7932e593cddb2654
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-16 02:23:32 -0700

    Calculate person age in years, months, and days

---
+++ csharp-challenge/BirthdayCalculationApplication/ConsoleUI/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Person
+    {
+        public DateTime Birthday { get; set; }
+
+        public int AgeInYears
+        {
+            get
+            {
+                return Convert.ToInt32(DateTime.Now.Subtract(Birthday).TotalDays) / 365;
+            }
+        }
+
+        public int AgeInMonths
+        {
+            get
+            {
+                return CalculateAgeInMonths();
+            }
+        }
+
+        public int AgeInDays
+        {
+            get
+            {
+                return Convert.ToInt32(DateTime.Now.Subtract(Birthday).TotalDays);
+            }
+        }
+
+        public Person(DateTime birthday)
+        {
+            Birthday = birthday;
+        }
+        
+        private int CalculateAgeInMonths()
+        {
+            DateTime dateNow = DateTime.Now;
+            DateTime tempDate = Birthday;
+            int months = 0;
+
+            if (Birthday.Year == dateNow.Year)
+            {
+                return dateNow.Month - Birthday.Month;
+            }
+
+            while (!(tempDate.Year == dateNow.Year && tempDate.Month == dateNow.Month))
+            {
+                tempDate = tempDate.AddMonths(1);
+                months++;
+            }
+
+            if (tempDate.Day > dateNow.Day)
+            {
+                months--;
+            }
+
+            return months;
+        }
+    }
+}
--- csharp-challenge/BirthdayCalculationApplication/ConsoleUI/Program.cs
+++ csharp-challenge/BirthdayCalculationApplication/ConsoleUI/Program.cs
@@ -7,9 +7,33 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            bool quit = false;
+
+            while (!quit)
+            {
+                Console.Write("\nEnter quit to end the program else press the enter key to continue: ");
+                string input = Console.ReadLine();
+
+                if (input == "quit")
+                {
+                    quit = true;
+                }
+                else
+                {
+                    RunBirthday();
+                }
+            }
+        }
+
+        static void RunBirthday()
+        {
             DateTime birthday = GetBirthdate();
+            Person person = new Person(birthday);
 
             Console.WriteLine(birthday.ToString());
+            Console.WriteLine($"The user is { person.AgeInYears } years old");
+            Console.WriteLine($"The user is { person.AgeInMonths } months old");
+            Console.WriteLine($"The user is { person.AgeInDays } days old");
         }
 
         static DateTime GetBirthdate()
