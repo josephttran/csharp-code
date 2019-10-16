a8074976ea17f9b5075ff20f0e8049353f3a844b
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-16 00:22:07 -0700

    Start birthday calculation challenge, get birthday in MM/dd/yyyy format from user

---
+++ csharp-challenge/BirthdayCalculationApplication/ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime birthday = GetBirthdate();
+
+            Console.WriteLine(birthday.ToString());
+        }
+
+        static DateTime GetBirthdate()
+        {
+            bool valid = false;
+            DateTime birthday = DateTime.Now;
+
+            while (!valid)
+            {
+                Console.Write("\nEnter your birthdate (MM/dd/yyyy): ");
+                string birthdayInput = Console.ReadLine();
+
+                if (DateTime.TryParseExact(birthdayInput, "MM/dd/yyyy", null, DateTimeStyles.None, out birthday))
+                {
+                    if (birthday.CompareTo(DateTime.Now) < 0)
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: birthday cannot be in the future!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: unable to process birthday entered!");
+                }
+            }
+
+            return birthday;
+        }
+    }
+}
