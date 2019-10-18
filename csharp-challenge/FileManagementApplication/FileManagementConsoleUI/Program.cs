1ad579d9231657ac376e827750580118506cb358
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-17 22:37:05 -0700

    Copy all sub-folders and files

--- csharp-challenge/FileManagementApplication/FileManagementConsoleUI/Program.cs
+++ csharp-challenge/FileManagementApplication/FileManagementConsoleUI/Program.cs
@@ -11,7 +11,8 @@ namespace FileManagementConsoleUI
             string sourceDirectoryPath = @"..\..\..\Folder1";
             string targetDirectoryPath = @"..\..\..\Folder2";
 
-            CopyAllFiles(sourceDirectoryPath, targetDirectoryPath);
+            //CopyAllFiles(sourceDirectoryPath, targetDirectoryPath);
+            CopyAllSubFoldersAndFiles(sourceDirectoryPath, targetDirectoryPath);
         }
 
         static void CopyAllFiles(string source, string target)
@@ -28,6 +29,53 @@ namespace FileManagementConsoleUI
                 foreach (string file in files)
                 {
                     string fileName = file.Substring(source.Length + 1);
+                    Console.WriteLine(fileName);
+                    string sourceFilePath = Path.Combine(source, fileName);
+                    string targetFilePath = Path.Combine(target, fileName);
+
+                    if (!File.Exists(targetFilePath))
+                    {
+                        File.Copy(sourceFilePath, targetFilePath);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        static void CopyAllSubFoldersAndFiles(string source, string target)
+        {
+            DirectoryInfo root = new DirectoryInfo(source);
+            List<DirectoryInfo> directories = new List<DirectoryInfo>();
+            List<FileInfo> filesPath = new List<FileInfo>();
+
+            TraverseSubDirectories(root, directories, filesPath);
+
+            // Create directories for copy files
+            if (directories != null)
+            {
+                foreach (var directory in directories)
+                {
+                    string sourceDirectoryFullPath = Path.GetFullPath(source);
+                    string sourceDirectoryRelativePath = directory.FullName.Substring(sourceDirectoryFullPath.Length + 1);
+                    string targetDirectoryPath = Path.Combine(Path.GetFullPath(target), sourceDirectoryRelativePath);
+
+                    if (!Directory.Exists(targetDirectoryPath))
+                    {
+                        Directory.CreateDirectory(targetDirectoryPath);
+                    }
+                }
+            }
+
+            // Copy files
+            if (filesPath != null)
+            {
+                foreach (var file in filesPath)
+                {
+                    string sourceDirectoryPath = Path.GetFullPath(source);
+                    string fileName = file.FullName.Substring(sourceDirectoryPath.Length + 1);
 
                     string sourceFilePath = Path.Combine(source, fileName);
                     string targetFilePath = Path.Combine(target, fileName);
@@ -36,6 +84,29 @@ namespace FileManagementConsoleUI
                     {
                         File.Copy(sourceFilePath, targetFilePath);
                     }
+                }
+            }
+        }
+
+        // Recursive get all sub directories and files
+        static void TraverseSubDirectories(DirectoryInfo root, List<DirectoryInfo> directories, List<FileInfo> filesPath)
+        {
+            try
+            {
+                DirectoryInfo[] subDirectories = root.GetDirectories();
+
+                if (root.GetFiles() != null)
+                {
+                    foreach (var file in root.GetFiles())
+                    {
+                        filesPath.Add(file);
+                    }
+                }
+
+                foreach (var subDirectory in subDirectories)
+                {
+                    directories.Add(subDirectory);
+                    TraverseSubDirectories(subDirectory, directories, filesPath);
                 }
             }
             catch (Exception exception)
