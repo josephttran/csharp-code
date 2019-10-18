15b88c8db8d1fffa8bed588ae15606b655b94567
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-17 18:11:38 -0700

    Copy all files from one folder to another folder

---
+++ csharp-challenge/FileManagementApplication/FileManagementConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileManagementConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceDirectoryPath = @"..\..\..\Folder1";
+            string targetDirectoryPath = @"..\..\..\Folder2";
+
+            CopyAllFiles(sourceDirectoryPath, targetDirectoryPath);
+        }
+
+        static void CopyAllFiles(string source, string target)
+        {
+            try
+            {
+                IEnumerable<string> files = Directory.EnumerateFiles(source);
+
+                if (!Directory.Exists(target))
+                {
+                    Directory.CreateDirectory(target);
+                }
+
+                foreach (string file in files)
+                {
+                    string fileName = file.Substring(source.Length + 1);
+
+                    string sourceFilePath = Path.Combine(source, fileName);
+                    string targetFilePath = Path.Combine(target, fileName);
+
+                    if (!File.Exists(targetFilePath))
+                    {
+                        File.Copy(sourceFilePath, targetFilePath);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
