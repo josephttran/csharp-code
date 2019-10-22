6ea6bc36c89a2768e33e7098a182c223d53ae500
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-21 17:17:11 -0700

    Add solution to generics challenge, create new list from two lists of different type

---
+++ csharp-challenge/GenericsApplication/GenericsConsoleUI/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int> { 2, 4, 6, 8, 10 };
+            List<string> wordList = new List<string> { "Fruit", "Vegetable", "Meat" };
+
+            List<object> mixList = InterMixLists<string, int>(wordList, numberList);
+
+            DisplayList(mixList);
+        }
+
+        static void DisplayList(List<object> list)
+        {
+            foreach (object item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static List<object> InterMixLists<T, K>(in List<T> firstList, in List<K> secondList)
+        {
+            List<object> mixList = new List<object>();
+
+            if (firstList.Count >= secondList.Count)
+            {
+                List<K>.Enumerator secondListEnumerator = secondList.GetEnumerator();
+
+                foreach (T item in firstList)
+                {
+                    mixList.Add(item);
+
+                    if (secondListEnumerator.MoveNext())
+                    {
+                        mixList.Add(secondListEnumerator.Current);
+                    }
+                }
+            }
+            else
+            {
+                List<T>.Enumerator firstListEnumerator = firstList.GetEnumerator();
+
+                foreach (K item in secondList)
+                {
+                    mixList.Add(item);
+
+                    if (firstListEnumerator.MoveNext())
+                    {
+                        mixList.Add(firstListEnumerator.Current);
+                    }
+                }
+            }
+
+            return mixList;
+        }
+    }
+}
