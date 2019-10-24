dd024c0ef2fe7ffa5237e79c6992eb8f12d5cfce
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-24 16:53:50 -0700

    Get people data from cache if exist

--- csharp-challenge/CachingData/CachingChallenge/PersonModel.cs
+++ csharp-challenge/CachingData/CachingChallenge/PersonModel.cs
@@ -5,5 +5,10 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ Id } { LastName }, { FirstName }";
+        }
     }
 }
--- csharp-challenge/CachingData/CachingChallenge/Program.cs
+++ csharp-challenge/CachingData/CachingChallenge/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CachingChallenge
 {
@@ -7,7 +9,51 @@ namespace CachingChallenge
     {
         static void Main(string[] args)
         {
-            
+            using (IMemoryCache memoryCache = new MemoryCache(
+                new MemoryCacheOptions
+                {
+                    SizeLimit = 6
+                }))
+            {
+                bool askForData = false;
+                string userInput;
+                DataAccess dataAccess = new DataAccess();
+                List<PersonModel> people = new List<PersonModel>();
+
+                while (!askForData)
+                {
+                    Console.Write("\nEnter quit to end the program else hit enter: ");
+                    userInput = Console.ReadLine();
+
+                    if (userInput.ToLower() == "quit")
+                    {
+                        break;
+                    }
+
+                    if (memoryCache.TryGetValue("People Data", out List<PersonModel> cachePeople))
+                    {
+                        people = cachePeople;
+                        Console.WriteLine("Data from cache");
+                    }
+                    else
+                    {
+                        people = dataAccess.SimulatedPersonListLookup();
+
+                        MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions()
+                            .SetSize(1)
+                            .SetAbsoluteExpiration(TimeSpan.FromSeconds(10));
+
+                        memoryCache.Set("People Data", people, cacheEntryOptions);
+                    }
+
+                    foreach (var personModel in people)
+                    {
+                        Console.WriteLine(personModel.ToString());
+                    }
+                }
+            }
+
+            Console.WriteLine("Application has ended");
             Console.ReadLine();
         }
     }
