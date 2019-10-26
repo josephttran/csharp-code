008001986336da1257d26017e1c857cfde8da786
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-25 19:23:22 -0700

    Add unit testing challenge starter code

---
+++ csharp-challenge/UnitTesting/CalculationsLibrary/TextDataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CalculationsLibrary
+{
+    public class TextDataAccess
+    {
+        public void SaveText(string filePath, List<string> lines)
+        {
+            if (filePath.Length > 260)
+            {
+                throw new PathTooLongException("The path needs to be less than 261 characters long.");
+            }
+
+            string fileName = Path.GetFileName(filePath);
+
+            File.WriteAllLines(fileName, lines);
+        }
+    }
+}
