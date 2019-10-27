8a7ebc790e33ad698fa4ea8c34d1a21e3a2059fe
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-27 01:55:45 -0700

    Add solution to razor pages in ASP.NET Core challenge

---
+++ csharp-challenge/RazorPagesApplication/AspDotNetCoreRazorPages/Pages/SayHi.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspDotNetCoreRazorPages.Pages
+{
+    public class SayHiModel : PageModel
+    {
+        [BindProperty]
+        [Required(ErrorMessage ="A name is required")]
+        [MinLength(2, ErrorMessage = "Must be  at least 2 characters")]
+        public string PersonName { get; set; }
+
+        public void OnGet() { }
+
+        public void OnPost()
+        {
+        }
+    }
+}
