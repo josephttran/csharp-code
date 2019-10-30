4b7b386d7ff40b6a363e6548432d757bda6a3c50
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-30 14:41:01 -0700

    Add solution to calculation challenege

---
+++ csharp-challenge/CalculationApplication/ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using TemperatureLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Temperature temperature = new Temperature();
+
+            // Insert 100 random between 1 and 100 inclusive
+            for (int i = 0; i < 101; i++)
+            {
+                int randomTemperature = random.Next(1, 101);
+
+                temperature.Insert(randomTemperature);
+            }
+
+            Console.WriteLine("Temperature minimum: " + temperature.Minimum);
+            Console.WriteLine("Temperature maximum: " + temperature.Maximum);
+            Console.WriteLine("Temperature average: " + temperature.Average);
+            Console.WriteLine();
+            Console.WriteLine("List of temperatures: ");
+            Console.WriteLine(String.Join(", ", temperature.Temperatures.ToArray()));
+        }
+    }
+}
---
+++ csharp-challenge/CalculationApplication/TemperatureLibrary/Temperature.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TemperatureLibrary
+{
+    public class Temperature
+    {
+        public List<int> Temperatures { get; private set; }
+        public double? Average { get; private set; } = null;
+        public int? Maximum { get; private set; } = null;
+        public int? Minimum { get; private set; } = null;
+
+        public Temperature()
+        {
+            Temperatures = new List<int>();
+        }
+
+        public void Insert(int temperature)
+        {
+            Temperatures.Add(temperature);
+
+            SetMinimum(temperature);
+            SetMaximum(temperature);
+            CalculateAverage();
+        }
+
+        private void CalculateAverage()
+        {
+            Average = Math.Round((double)Temperatures.Sum() / Temperatures.Count, 2);
+        }
+
+        private void SetMaximum(int temperature)
+        {
+            if (!Maximum.HasValue)
+            {
+                Maximum = temperature;
+            }
+            else
+            {
+                if (Temperatures.Count == 0 || Maximum.Value.CompareTo(temperature) < 0)
+                {
+                    Maximum = temperature;
+                }
+            }
+        }
+
+        private void SetMinimum(int temperature)
+        {
+            if (!Minimum.HasValue)
+            {
+                Minimum = temperature;
+            }
+            else
+            {
+                if (Minimum.Value.CompareTo(temperature) > 0)
+                {
+                    Minimum = temperature;
+                }
+            }
+        }
+    }
+}
