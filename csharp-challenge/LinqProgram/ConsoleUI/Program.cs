98123747b62ad76469f44ad09793514febdf7e5b
josephttran <42635012+josephttran@users.noreply.github.com>
2019-10-31 16:03:33 -0700

    Add solution to LINQ primary challenge

---
+++ csharp-challenge/LinqProgram/ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = "Hello World";
+            List<char> orderedLetters = StringToOrderedLetters(words);
+
+            Console.WriteLine(words);
+            Console.WriteLine(String.Join(", ", orderedLetters.ToArray()));
+        }
+
+        static List<char> StringToOrderedLetters(string words)
+        {
+            return words.Where(character => char.IsLetter(character))
+                        .OrderBy(character => character.ToString().ToLower())
+                        .ToList();
+        }
+    }
+}
