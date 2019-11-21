83724b62695897282f8686c880ddf31d750dae89
josephttran <42635012+josephttran@users.noreply.github.com>
2019-11-20 20:30:05 -0800

    Replace all instances of a given word based on user input

--- csharp-challenge/StringReplacementApplication/ConsoleUI/Program.cs
+++ csharp-challenge/StringReplacementApplication/ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ConsoleUI
 {
@@ -6,7 +8,32 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string primaryTextPath = @"../../../primary.txt";
+            string replacedPrimaryTextPath = @"../../../replacedPrimary.txt";
+
+            Primary(primaryTextPath, replacedPrimaryTextPath);
+        }
+
+        static void Primary(string primaryTextPath, string replacedPrimaryTextPath)
+        {
+            if (!File.Exists(primaryTextPath))
+            {
+                Console.WriteLine($"File not found {primaryTextPath}");
+            }
+            else
+            {
+                Console.Write("What text would you like to replace? ");
+                string textToReplace = Console.ReadLine();
+
+                Console.Write("Now what do you want to replace it with? ");
+                string replaceWith = Console.ReadLine();
+
+                string primaryText = File.ReadAllText(primaryTextPath);
+                string pattern = @$"\b{ textToReplace }\b";
+                string replacedPrimaryText = Regex.Replace(primaryText, pattern, replaceWith);
+
+                File.WriteAllText(replacedPrimaryTextPath, replacedPrimaryText);
+            }
         }
     }
 }
