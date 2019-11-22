6520ae96c94bef00fe2e350ad296417127c46d44
josephttran <42635012+josephttran@users.noreply.github.com>
2019-11-21 19:50:59 -0800

    Add solution to extension methods primary challenge

---
+++ csharp-challenge/ExtensionMethods/ConsoleUI/ExtensionMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    public static class ExtensionMethod
+    {
+        public static void Print(this string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public static void Excite(this string str)
+        {
+            string newString = str.Replace(".", "!");
+
+            Console.WriteLine(newString);
+        }
+    }
+}
---
+++ csharp-challenge/ExtensionMethods/ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "Hello World.".Print();
+            "Hello World.".Excite();
+        }
+    }
+}
