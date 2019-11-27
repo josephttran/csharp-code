50c225dd6818b475d883069b792b7569920c315d
josephttran <42635012+josephttran@users.noreply.github.com>
2019-11-26 22:57:17 -0800

    Add text files for searching for data challenge

---
+++ csharp-challenge/SearchingForDataApplication/WindowsFormsUI/SearchForDataUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsUI
+{
+    public partial class SearchForDataUI : Form
+    {
+        public SearchForDataUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
