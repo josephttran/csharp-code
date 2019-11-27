c1efdd885ebfd73ca57e209e48c96d1b4f4b81de
josephttran <42635012+josephttran@users.noreply.github.com>
2019-11-27 00:18:09 -0800

    Display each line from primary.txt that matches the search box

--- csharp-challenge/SearchingForDataApplication/WindowsFormsUI/SearchForDataUI.cs
+++ csharp-challenge/SearchingForDataApplication/WindowsFormsUI/SearchForDataUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace WindowsFormsUI
 {
     public partial class SearchForDataUI : Form
     {
+        readonly string primaryPath = @"../../Data/primary.txt";
+
         public SearchForDataUI()
         {
             InitializeComponent();
@@ -24,7 +27,22 @@ namespace WindowsFormsUI
 
         private void ButtonPrimarySearch_Click(object sender, EventArgs e)
         {
+            string searchValue = textBoxPrimarySearch.Text.ToLower();
 
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                listBoxPrimary.Items.Clear();
+
+                IEnumerable<string> lines = File.ReadLines(primaryPath);
+
+                foreach (string line in lines)
+                {
+                    if (line.ToLower().IndexOf(searchValue) > -1)
+                    {
+                        listBoxPrimary.Items.Add(line);
+                    }
+                }
+            }
         }
     }
 }
