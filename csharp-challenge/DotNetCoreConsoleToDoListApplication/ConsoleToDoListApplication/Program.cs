cd23da7e6d880bbdf0d763d0686b4f8a83297fc3
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-05 23:36:44 -0800

    Add command details

--- csharp-challenge/DotNetCoreConsoleToDoListApplication/ConsoleToDoListApplication/Program.cs
+++ csharp-challenge/DotNetCoreConsoleToDoListApplication/ConsoleToDoListApplication/Program.cs
@@ -25,6 +25,19 @@ namespace ConsoleToDoListApplication
             Console.WriteLine("\nEnter an action: ");
         }
 
+        static void ShowHelp()
+        {
+            Console.WriteLine($"\n{"Command", -15} Command Information");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine($"{"Add", -15} Add a todo task");
+            Console.WriteLine($"{"", -19} <todo> is your task");
+            Console.WriteLine($"{"Clear",-15} Remove all items in todo list");
+            Console.WriteLine($"{"Done",-15} Mark the task to done");
+            Console.WriteLine($"{"", -19} <todo number> is id of task");
+            Console.WriteLine($"{"Exit",-15} Quit the program");
+            Console.WriteLine($"{"Print", -15} Display top 3 task in todo list");
+            Console.WriteLine($"{"Print all", -15} Display all tasks in todo list");
+        }
 
     }
 }
