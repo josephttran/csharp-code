d12a02a941897d8865d56e9eb9a0560c391cda85
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-05 19:46:41 -0800

    Add menu for .NET Core console todo list challenge

---
+++ csharp-challenge/DotNetCoreConsoleToDoListApplication/ConsoleToDoListApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleToDoListApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("####################################");
+            Console.WriteLine("The Todo List Application");
+            Console.WriteLine("Here is a list of available actions:");
+            Console.WriteLine("####################################");
+            Console.WriteLine("Print");
+            Console.WriteLine("Print all");
+            Console.WriteLine("Add <todo>");
+            Console.WriteLine("Done <todo number>");
+            Console.WriteLine("Clear");
+            Console.WriteLine("Help");
+            Console.WriteLine("Exit");
+            Console.WriteLine("\nEnter an action: ");
+        }
+
+
+    }
+}
