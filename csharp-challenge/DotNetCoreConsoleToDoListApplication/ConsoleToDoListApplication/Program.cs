985d201bcfb444e09d492aac2e1f2689e634d8ab
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-06 01:19:50 -0800

    Implement actions

--- csharp-challenge/DotNetCoreConsoleToDoListApplication/ConsoleToDoListApplication/Program.cs
+++ csharp-challenge/DotNetCoreConsoleToDoListApplication/ConsoleToDoListApplication/Program.cs
@@ -1,17 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleToDoListApplication
 {
     class Program
     {
+        static List<TodoItem> todoList = new List<TodoItem>();
+
         static void Main(string[] args)
         {
             Menu();
         }
 
+        static void Add(TodoItem todoItem)
+        {
+            todoList.Add(todoItem);
+        }
+
+        static void Clear()
+        {
+            todoList.Clear();
+        }
+
+        static void Done(int id)
+        {
+            if (todoList[id] != null)
+            {
+                todoList[id].Done = true;
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid Id");
+            }
+        }
+
+        static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
         static void Menu()
         {
-            Console.WriteLine("####################################");
+            Console.WriteLine("\n####################################");
             Console.WriteLine("The Todo List Application");
             Console.WriteLine("Here is a list of available actions:");
             Console.WriteLine("####################################");
@@ -22,7 +52,37 @@ namespace ConsoleToDoListApplication
             Console.WriteLine("Clear");
             Console.WriteLine("Help");
             Console.WriteLine("Exit");
-            Console.WriteLine("\nEnter an action: ");
+            Console.Write("\nEnter an action: ");
+        }
+
+        static void Print()
+        {
+            if (todoList.Count > 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    todoList[i].Display();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Todo List is empty");
+            }
+        }
+
+        static void PrintAll()
+        {
+            if (todoList.Count > 0)
+            {
+                foreach (TodoItem todoItem in todoList)
+                {
+                    todoItem.Display();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Todo List is empty");
+            }
         }
 
         static void ShowHelp()
@@ -31,13 +91,12 @@ namespace ConsoleToDoListApplication
             Console.WriteLine("---------------------------------------------");
             Console.WriteLine($"{"Add", -15} Add a todo task");
             Console.WriteLine($"{"", -19} <todo> is your task");
-            Console.WriteLine($"{"Clear",-15} Remove all items in todo list");
-            Console.WriteLine($"{"Done",-15} Mark the task to done");
+            Console.WriteLine($"{"Clear", -15} Remove all items in todo list");
+            Console.WriteLine($"{"Done", -15} Mark the task to done");
             Console.WriteLine($"{"", -19} <todo number> is id of task");
-            Console.WriteLine($"{"Exit",-15} Quit the program");
+            Console.WriteLine($"{"Exit", -15} Quit the program");
             Console.WriteLine($"{"Print", -15} Display top 3 task in todo list");
             Console.WriteLine($"{"Print all", -15} Display all tasks in todo list");
         }
-
     }
 }
---
+++ csharp-challenge/DotNetCoreConsoleToDoListApplication/ConsoleToDoListApplication/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleToDoListApplication
+{
+    class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; } = false;
+
+        public void Display()
+        {
+            Console.WriteLine($"{Id} {Name}");
+            Console.WriteLine($"Done? {Done}");
+        }
+    }
+}
