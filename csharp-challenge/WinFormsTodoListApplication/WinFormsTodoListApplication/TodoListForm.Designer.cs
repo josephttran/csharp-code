ee9511fec3fa4806f7b46c82640047088bd2efe4
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-10 23:19:04 -0800

    Begin windows forms todo list challenge

---
+++ csharp-challenge/WinFormsTodoListApplication/WinFormsTodoListApplication/TodoListForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WinFormsTodoListApplication
+{
+    partial class TodoListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.TodoHeading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(51, 111);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(476, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // TodoHeading
+            // 
+            this.TodoHeading.AutoSize = true;
+            this.TodoHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TodoHeading.Location = new System.Drawing.Point(51, 49);
+            this.TodoHeading.Name = "TodoHeading";
+            this.TodoHeading.Size = new System.Drawing.Size(112, 26);
+            this.TodoHeading.TabIndex = 1;
+            this.TodoHeading.Text = "Todo Task";
+            // 
+            // TodoListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TodoHeading);
+            this.Controls.Add(this.listBox1);
+            this.Name = "TodoListForm";
+            this.Text = "Todo List Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label TodoHeading;
+    }
+}
+
---
+++ csharp-challenge/WinFormsTodoListApplication/WinFormsTodoListApplication/TodoListForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTodoListApplication
+{
+    public partial class TodoListForm : Form
+    {
+        public TodoListForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
