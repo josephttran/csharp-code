55f6a346d841c14e5f5b961839b789aac9677f3d
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-21 19:30:07 -0800

    Add person view and populate combobox with data from mongodb

--- csharp-challenge/MongoDbCrudWpfApplication/WpfApplication/ViewModels/PersonViewModel.cs
+++ csharp-challenge/MongoDbCrudWpfApplication/WpfApplication/ViewModels/PersonViewModel.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using WpfApplication.DataAccess;
+using WpfApplication.Models;
 
 namespace WpfApplication.ViewModels
 {
-    class PersonViewModel
+    public class PersonViewModel
     {
+        public ObservableCollection<PersonModel> People { get; set; }
+        public PersonModel SelectedPersonModel { get; set; }
+
+        public PersonViewModel()
+        {
+            People = new ObservableCollection<PersonModel>();
+            InitializePeople();
+        }
+
+        private async void InitializePeople()
+        {
+            PeopleRepository peopleRepository = new PeopleRepository();
+            List<PersonModel> personList = await peopleRepository.GetAll();
+
+            if (personList.Count > 0)
+            {
+                foreach (var person in personList)
+                {
+                    People.Add(person);
+                }
+            }
+        }
     }
 }
