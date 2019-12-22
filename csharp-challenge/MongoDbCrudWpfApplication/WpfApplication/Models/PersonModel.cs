5b7f206d89f949426b4bdb229b51c25951c7951c
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-21 18:35:51 -0800

    Add person model

--- csharp-challenge/MongoDbCrudWpfApplication/WpfApplication/Models/PersonModel.cs
+++ csharp-challenge/MongoDbCrudWpfApplication/WpfApplication/Models/PersonModel.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace WpfApplication.Models
 {
-    class PersonModel
+    public class PersonModel
     {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("firstname")]
         public string FirstName { get; set; }
+
+        [BsonElement("lastname")]
         public string LastName { get; set; }
+
+        [BsonElement("email")]
         public string Email { get; set; }
     }
 }
