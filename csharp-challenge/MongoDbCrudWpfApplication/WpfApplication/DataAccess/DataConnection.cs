9effe8f48a4dc6ae0c8ad693a312955dbfd178f6
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-21 18:33:19 -0800

    Connect to MongoDB database and collection

---
+++ csharp-challenge/MongoDbCrudWpfApplication/WpfApplication/DataAccess/DataConnection.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using WpfApplication.Models;
+
+namespace WpfApplication.DataAccess
+{
+    public class DataConnection
+    {
+        private readonly string dbName = "csharp";
+        protected IMongoDatabase Database { get; set; }
+
+        protected DataConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            MongoClient Client = new MongoClient(connectionString);
+            Database = Client.GetDatabase(dbName);
+        }
+    }
+}
---
+++ csharp-challenge/MongoDbCrudWpfApplication/WpfApplication/DataAccess/PeopleRepository.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplication.Models;
+
+namespace WpfApplication.DataAccess
+{
+    public class PeopleRepository : DataConnection
+    {
+        private readonly IMongoCollection<PersonModel> peopleCollection;
+
+        public PeopleRepository()
+        {
+            string dbCollectionName = "people";
+            peopleCollection = Database.GetCollection<PersonModel>(dbCollectionName);
+        }
+
+        public async Task<List<PersonModel>> GetAll()
+        {
+            return await peopleCollection.Find(new BsonDocument()).ToListAsync();
+        }
+    }
+}
