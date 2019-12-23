54e8126fd9d7bea79a8e14f7199fb17ab792c764
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-22 17:02:03 -0800

    Add view model base class and add class that implement ICommand interface

---
+++ csharp-challenge/MongoDbCrudWpfApplication/WpfApplication/ViewModels/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfApplication.ViewModels
+{
+    class RelayCommand : ICommand
+    {
+        readonly Action<object> _executeMethod;
+        readonly Func<object, bool> _canExecuteMethod;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public RelayCommand(Action<object> executeMethod, Func<object, bool> canExecuteMethod)
+        {
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteMethod(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod(parameter);
+        }
+    }
+}
