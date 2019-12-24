f57696a59b3da5f5645206c84a5a9478c8f68143
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-23 19:32:33 -0800

    Begin .NET Core API todo list with MongoDB challenge

---
+++ csharp-challenge/DotNetCoreApiTodoListWithMongoDbApplication/DotNetCoreApiTodoListWithMongoDbApplication/Controllers/TodosController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreApiTodoListWithMongoDbApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodosController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // PATCH api/<controller>/5
+        [HttpPatch]
+        public void Patch(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ csharp-challenge/DotNetCoreApiTodoListWithMongoDbApplication/DotNetCoreApiTodoListWithMongoDbApplication/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetCoreApiTodoListWithMongoDbApplication.Models
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
