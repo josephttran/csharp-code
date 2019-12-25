d1ae9e260b23c9ad1bbd6b0decdde9f95300eac5
josephttran <42635012+josephttran@users.noreply.github.com>
2019-12-25 01:02:57 -0800

    Add Swagger/OpenAPI display for todos API

--- csharp-challenge/DotNetCoreApiTodoListWithMongoDbApplication/DotNetCoreApiTodoListWithMongoDbApplication/Startup.cs
+++ csharp-challenge/DotNetCoreApiTodoListWithMongoDbApplication/DotNetCoreApiTodoListWithMongoDbApplication/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace DotNetCoreApiTodoListWithMongoDbApplication
 {
@@ -28,6 +29,12 @@ namespace DotNetCoreApiTodoListWithMongoDbApplication
             services.AddSingleton<DataService>();
 
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +44,17 @@ namespace DotNetCoreApiTodoListWithMongoDbApplication
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
