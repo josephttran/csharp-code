7fddbc7e13f6cfafab7e1d602764b2de6134fbff
josephttran <42635012+josephttran@users.noreply.github.com>
2020-01-06 21:45:28 -0800

    Add tables to hold users and todos

---
+++ csharp-challenge/SqlTodoListWithSqlServerDataToolsProject/SqlTodoDatabaseProject/Dbo/Tables/Person.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL
+)
---
+++ csharp-challenge/SqlTodoListWithSqlServerDataToolsProject/SqlTodoDatabaseProject/Dbo/Tables/TodoItem.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TodoItem]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [BelongsTo] INT NOT NULL,
+    [Name] NVARCHAR(50) NOT NULL, 
+    [IsCompleted] BIT NULL, 
+    CONSTRAINT [FK_Todo_Person] FOREIGN KEY ([BelongsTo]) REFERENCES [Person]([Id])
+)
