f88e7ec8bc1431eb8be8c4fabbaac6b124256ea6
josephttran <42635012+josephttran@users.noreply.github.com>
2020-01-11 11:09:08 -0800

    Create lookup store procedure

--- csharp-challenge/SqlTodoListWithSqlServerDataToolsProject/SqlTodoDatabaseProject/Dbo/StoredProcedures/spLookup.sql
+++ csharp-challenge/SqlTodoListWithSqlServerDataToolsProject/SqlTodoDatabaseProject/Dbo/StoredProcedures/spLookup.sql
@@ -1,1 +1,11 @@
-﻿
+﻿CREATE PROCEDURE [dbo].[spLookup]
+	@id int
+AS
+BEGIN
+	SELECT Person.Id, FirstName, LastName, RecurringTodoItem.ItemDescription, RecurringTodoItem.StartDate, IsActive, IsCompleted
+	FROM Person INNER JOIN RecurringTodoItem
+	ON Person.Id = RecurringTodoItem.BelongsTo
+	WHERE Person.Id = @id
+	AND IsActive = 1
+	AND IsCompleted = 0;
+END
