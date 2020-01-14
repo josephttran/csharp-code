cfa3b9bf24b2725e29a2bc28fcb9e32f5add243e
josephttran <42635012+josephttran@users.noreply.github.com>
2020-01-13 22:30:45 -0800

    Show recurring todo items total completed, total not completed, and total assigned

--- csharp-challenge/SqlTodoListWithSqlServerDataToolsProject/SqlTodoDatabaseProject/Dbo/StoredProcedures/spHistory.sql
+++ csharp-challenge/SqlTodoListWithSqlServerDataToolsProject/SqlTodoDatabaseProject/Dbo/StoredProcedures/spHistory.sql
@@ -1,1 +1,30 @@
-﻿
+﻿CREATE PROCEDURE [dbo].[spHistory]
+AS
+BEGIN
+	SELECT C.ItemDescription, C.[Total Not Completed], D.[Total Completed], (C.[Total Not Completed] + D.[Total Completed]) AS 'Total Assigned'
+	FROM
+		(
+			SELECT RecurringTodoItem.ItemDescription, COUNT (*) AS 'Total Not Completed'
+			FROM  RecurringTodoItem
+			GROUP BY RecurringTodoItem.ItemDescription
+		) C
+		LEFT JOIN
+		(
+			SELECT A.ItemDescription, (CASE WHEN TimeCompleted IS NULL THEN 0 ELSE TimeCompleted END) AS 'Total Completed'
+			FROM
+			(
+				SELECT RecurringTodoItem.ItemDescription
+				FROM RecurringTodoItem INNER JOIN RecurringTodoCompletedHistory
+				ON RecurringTodoItemId != RecurringTodoItem.Id
+				GROUP BY RecurringTodoItem.ItemDescription
+			) A
+			LEFT JOIN
+			(SELECT RecurringTodoItem.ItemDescription, COUNT (*) AS TimeCompleted
+			FROM RecurringTodoItem INNER JOIN RecurringTodoCompletedHistory
+			ON RecurringTodoItemId = RecurringTodoItem.Id
+			GROUP BY RecurringTodoItem.ItemDescription
+			) B
+		ON a.ItemDescription = b.ItemDescription
+		) D
+	ON C.ItemDescription = D.ItemDescription
+END
