9bd72275b3f230eb75e249d994a501c42ebc401d
josephttran <42635012+josephttran@users.noreply.github.com>
2020-01-29 20:36:00 -0800

    Rename uploaded file to GUID with same extension and save the file to Uploads folder

--- csharp-challenge/FileUploadsWithAspDotNetCoreRazorPages/WebFileUploads/Pages/FileUploads/Index.cshtml.cs
+++ csharp-challenge/FileUploadsWithAspDotNetCoreRazorPages/WebFileUploads/Pages/FileUploads/Index.cshtml.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using WebFileUploads.Models;
 
 namespace WebFileUploads.Pages.FileUploads
 {
     public class IndexModel : PageModel
     {
+        private readonly long _fileSizeLimit;
+        private readonly string _saveFilePath;
+
+        public IndexModel(IConfiguration config)
+        {
+            _fileSizeLimit = config.GetValue<long>("FileSizeLimit");
+            _saveFilePath = config.GetValue<string>("SaveFilePath");
+        }
+
         [BindProperty]
         public FileUpload FileUpload { get; set; }
         public void OnGet()
@@ -18,6 +30,35 @@ namespace WebFileUploads.Pages.FileUploads
 
         public async Task<IActionResult> OnPostUploadAsync()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            if (FileUpload.FormFile.Length < _fileSizeLimit)
+            {
+                Guid guid = Guid.NewGuid();
+                string fileName = FileUpload.FormFile.FileName;
+                string fileExtension = Path.GetExtension(fileName);
+                string newFileName = $"{guid}{fileExtension}";
+                string newFilePath = Path.Combine(_saveFilePath, newFileName);
+
+                if (!Directory.Exists(_saveFilePath))
+                {
+                    Directory.CreateDirectory(_saveFilePath);
+                }
+
+                IFormFile formFile = FileUpload.FormFile;
+
+                using (var stream = System.IO.File.Create(newFilePath))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+            }
+
+            Console.WriteLine(FileUpload);
+
             return Page();
         }
     }
