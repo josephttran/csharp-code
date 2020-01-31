52ea8d7f57e259fb86408544dfc4a6943b94a86f
josephttran <42635012+josephttran@users.noreply.github.com>
2020-01-30 18:20:31 -0800

    Replace every name of files to standard casing and if contain Acme replace it with TimCo

--- csharp-challenge/BulkFileRenaming/ConsoleApplication/Program.cs
+++ csharp-challenge/BulkFileRenaming/ConsoleApplication/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleApplication
 {
@@ -6,7 +8,49 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("!");
+            string primaryFilesDirectoryPath = @"..\..\..\PrimaryChallengeFiles";
+            string renamePrimaryFilesDirectoryPath = @"..\..\..\RenamePrimaryChallengeFiles";
+
+            RenamePrimaryChallengeFiles(primaryFilesDirectoryPath, renamePrimaryFilesDirectoryPath);
+
+            Console.WriteLine("Renaming done");
         }
+
+        static void RenamePrimaryChallengeFiles(string primaryFilesDirectoryPath, string renamePrimaryFilesDirectoryPath)
+        {
+            string search = "acme";
+            string replaceWith = "TimCo";
+            IEnumerable<string> files = Directory.EnumerateFiles(primaryFilesDirectoryPath);
+
+            if (files != null)
+            {
+                if (!Directory.Exists(renamePrimaryFilesDirectoryPath))
+                {
+                    Directory.CreateDirectory(renamePrimaryFilesDirectoryPath);
+                }
+
+                foreach (var file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    string fileName = Path.GetFileNameWithoutExtension(fileInfo.Name);
+                    string extension = fileInfo.Extension;
+
+                    string renameFile = StringRename.ToTitleCase(fileName);
+                    renameFile = StringRename.ReplaceExact(renameFile, search, replaceWith);
+                    renameFile = renameFile + extension;
+
+                    string renameFilePath = Path.Combine(renamePrimaryFilesDirectoryPath, renameFile);
+
+                    if (File.Exists(renameFilePath))
+                    {
+                        File.Delete(renameFilePath);
+                    }
+
+                    fileInfo.CopyTo(renameFilePath, true);
+                }
+            }
+        }
+
+
     }
 }
---
+++ csharp-challenge/BulkFileRenaming/ConsoleApplication/StringRename.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication
+{
+    public class StringRename
+    {
+        public static string ToTitleCase(string text)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            string titleCaseText = textInfo.ToTitleCase(text.ToLower());
+
+            return titleCaseText;
+        }
+
+        public static string ReplaceExact(string text, string search, string replaceWith)
+        {
+            string pattern = $"\\b{search}\\b";
+
+            return Regex.Replace(text, pattern, replaceWith, RegexOptions.IgnoreCase);
+        }
+    }
+}
