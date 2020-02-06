83d25e294396ecd7827cce80947ecff052a397b6
josephttran <42635012+josephttran@users.noreply.github.com>
2020-02-05 23:58:11 -0800

    Implement a method to return a random integer in a range

--- csharp-challenge/CreatingSampleData/SampleDataLibrary/SampleData.cs
+++ csharp-challenge/CreatingSampleData/SampleDataLibrary/SampleData.cs
@@ -12,5 +12,12 @@ namespace SampleDataLibrary
             bool randomBool = random.NextDouble() > 0.5;
             return randomBool;
         }
+
+        public static int CreateRandomInteger(int minRange, int maxRange)
+        {
+            Random random = new Random();
+            int randomInteger = random.Next(minRange, maxRange + 1);
+            return randomInteger;
+        }
     }
 }
