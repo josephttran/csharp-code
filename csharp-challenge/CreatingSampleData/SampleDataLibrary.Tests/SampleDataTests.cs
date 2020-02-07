054567c195e1e69dfa1321b88e84cdf4320ab338
josephttran <42635012+josephttran@users.noreply.github.com>
2020-02-06 16:59:55 -0800

    Add Nunit tests

---
+++ csharp-challenge/CreatingSampleData/SampleDataLibrary.Tests/SampleDataTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace SampleDataLibrary.Tests
+{
+    [TestFixture]
+    public class SampleDataTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestCreateRandomBool()
+        {
+            var randomBool = SampleData.CreateRandomBoolean();
+
+            if (randomBool == true || randomBool == false)
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
