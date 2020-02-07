642b540aeab74f5b56521dfe156faa4336a5125d
josephttran <42635012+josephttran@users.noreply.github.com>
2020-02-06 17:16:54 -0800

    Add tests for CreateRandomInteger

--- csharp-challenge/CreatingSampleData/SampleDataLibrary.Tests/SampleDataTests.cs
+++ csharp-challenge/CreatingSampleData/SampleDataLibrary.Tests/SampleDataTests.cs
@@ -25,5 +25,15 @@ namespace SampleDataLibrary.Tests
                 Assert.Fail();
             }
         }
+
+        [TestCase(1, 100)]
+        [TestCase(-1, 1)]
+        [TestCase(0, 1)]
+        public void TestCreateRandomIntegerGivenMinMaxRange(int min, int max)
+        {
+            var randomInteger = SampleData.CreateRandomInteger(min, max);
+
+            Assert.IsTrue(randomInteger >= min && randomInteger <= max);
+        }
     }
 }
