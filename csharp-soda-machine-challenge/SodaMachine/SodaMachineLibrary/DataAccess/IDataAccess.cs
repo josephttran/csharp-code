e8a5d0f7c227b56468248b81bd32f3cdfc5d1d18
josephttran <42635012+josephttran@users.noreply.github.com>
2020-04-07 22:46:57 -0700

    Add interface for data access

---
+++ csharp-soda-machine-challenge/SodaMachine/SodaMachineLibrary/DataAccess/IDataAccess.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using SodaMachine.Models;
+
+namespace SodaMachine.DataAccess
+{
+    public interface IDataAccess
+    {
+        void CoinInventoryAddCoins(List<CoinModel> coins);
+        List<CoinModel> CoinInventoryGetAll();
+        List<CoinModel> CoinInventoryWithdrawCoins(decimal coinValue, int quantity);
+        decimal MachineInfoCashOnHand();
+        decimal MachineInfoEmptyCash();
+        decimal MachineInfoSodaPrice();
+        decimal MachineInfoTotalIncome();
+        void SodaInventoryAddSodas(List<SodaModel> sodas);
+        List<SodaModel> SodaInventoryGetAll();
+        SodaModel SodaInventoryGetSoda(SodaModel soda);
+        List<SodaModel> SodaInventoryGetTypes();
+        void UserCreditClear(string userId);
+        void UserCreditDeposit(string userId);
+        void UserCreditInsert(string userId, decimal amount);
+        decimal UserCreditTotal(string userId);
+
+    }
+}
