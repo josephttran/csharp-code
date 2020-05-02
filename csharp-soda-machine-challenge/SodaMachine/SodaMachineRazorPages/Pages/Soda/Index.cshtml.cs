a83b18948344799fd3f23c02d19937a9b03e7c31
josephttran <42635012+josephttran@users.noreply.github.com>
2020-05-01 20:00:03 -0700

    Bind drop down list and handle soda request

--- csharp-soda-machine-challenge/SodaMachine/SodaMachineRazorPages/Pages/Soda/Index.cshtml.cs
+++ csharp-soda-machine-challenge/SodaMachine/SodaMachineRazorPages/Pages/Soda/Index.cshtml.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SodaMachineLibrary.Logics;
 using SodaMachineLibrary.Models;
 
@@ -13,14 +16,18 @@ namespace SodaMachineRazorPages.Pages.Soda
     {
         private readonly ISodaMachineLogic _sodaMachineLogic;
 
-        public List<decimal> AcceptedCoinValues { get; set; }
+        public List<decimal> AcceptedCoinsValue { get; set; }
         public decimal ChangeReturned { get; set; }
         public string RequestSodaMessage { get; set; }
+        [BindProperty]
         public decimal SelectedCoinValue { get; set; }
-        public SodaModel SelectedSoda { get; set; }
+        [BindProperty]
+        public string SelectedSodaIndex { get; set; }
         public decimal SodaPrice { get; set; }
         public List<SodaModel> SodasInStock { get; set; }
+        public List<SelectListItem> SodaSelectList { get; set; }
         public List<SodaModel> TypesOfSoda { get; set; }
+        [BindProperty]
         public string UserId { get; set; }
         public decimal UserCredit { get; set; }
 
@@ -29,66 +36,84 @@ namespace SodaMachineRazorPages.Pages.Soda
             _sodaMachineLogic = sodaMachineLogic;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
             ViewData["Title"] = "Soda";
-
             UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            AcceptedCoinsValue = _sodaMachineLogic.AcceptedCoinValues;
+            SodaPrice = _sodaMachineLogic.GetSodaPrice();
+            SodasInStock = _sodaMachineLogic.GetSodaInventory();
+            TypesOfSoda = _sodaMachineLogic.ListTypesOfSoda();
+            UserCredit = _sodaMachineLogic.GetMoneyInsertedTotal(UserId);
 
-            Initialize();
+            SodaSelectList = SodasInStock.Select((sodaModel, index) =>
+            {
+                return new SelectListItem($"{sodaModel.Name} {sodaModel.SlotOccupied}", index.ToString());
+            }).ToList();
+
+            return Page();
         }
 
-        public void OnPostDepositCoins()
+        public IActionResult OnPostDepositCoins()
         {
-            if (AcceptedCoinValues.Contains(SelectedCoinValue))
+            AcceptedCoinsValue = _sodaMachineLogic.AcceptedCoinValues;
+
+            if (AcceptedCoinsValue.Contains(SelectedCoinValue))
             {
                 _sodaMachineLogic.MoneyInserted(UserId, SelectedCoinValue);
 
                 UserCredit = _sodaMachineLogic.GetMoneyInsertedTotal(UserId);
             }
+
+            return OnGet();
         }
 
-        public void OnPostEjectMoneyInserted()
+        public IActionResult OnPostEjectMoneyInserted()
         {
             UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             ChangeReturned = _sodaMachineLogic.IssueFullRefund(UserId);
-            UserCredit = 0;
+
+            return OnGet();
         }
 
-        public void OnPostRequestSoda()
+        public IActionResult OnPostRequestSoda()
         {
-            if (SelectedSoda != null)
+            if (int.TryParse(SelectedSodaIndex, out int sodaIndex))
             {
-                var (soda, coins, message) = _sodaMachineLogic.RequestSoda(SelectedSoda);
+                SodasInStock = _sodaMachineLogic.GetSodaInventory();
 
-                RequestSodaMessage = message;
-
-                if (coins.Count > 0)
+                if (sodaIndex > -1 && sodaIndex < SodasInStock.Count)
                 {
-                    decimal change = 0;
+                    SodaModel requestSoda = SodasInStock[sodaIndex];
 
-                    foreach (CoinModel coin in coins)
+                    var (soda, coins, message) = _sodaMachineLogic.RequestSoda(requestSoda);
+
+                    RequestSodaMessage = message;
+
+                    if (coins.Count > 0)
                     {
-                        change += coin.Amount;
-                    }
+                        decimal change = 0;
 
-                    ChangeReturned = change;
+                        foreach (CoinModel coin in coins)
+                        {
+                            change += coin.Amount;
+                        }
+
+                        ChangeReturned = change;
+                    }
+                }
+                else
+                {
+                    RequestSodaMessage = "Please select a soda";
                 }
             }
-            else
-            {
-                RequestSodaMessage = "No soda selected";
+            else 
+            { 
+                RequestSodaMessage = "Please select a soda";
             }
-        }
 
-        private void Initialize()
-        {
-            AcceptedCoinValues = _sodaMachineLogic.AcceptedCoinValues;
-            SodaPrice = _sodaMachineLogic.GetSodaPrice();
-            SodasInStock = _sodaMachineLogic.GetSodaInventory();
-            TypesOfSoda = _sodaMachineLogic.ListTypesOfSoda();
-            UserCredit = _sodaMachineLogic.GetMoneyInsertedTotal(UserId);
+            return OnGet();
         }
     }
 }
