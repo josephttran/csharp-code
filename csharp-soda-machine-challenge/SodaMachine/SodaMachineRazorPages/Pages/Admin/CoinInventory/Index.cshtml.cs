cecc394e57dad7de26f6a6792e7bf0da4ca493f7
josephttran <42635012+josephttran@users.noreply.github.com>
2020-05-07 02:03:29 -0700

    Add coin inventory page

---
+++ csharp-soda-machine-challenge/SodaMachine/SodaMachineRazorPages/Pages/Admin/CoinInventory/Index.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SodaMachineLibrary.DataAccess;
+using SodaMachineLibrary.Models;
+
+namespace SodaMachineRazorPages.Pages.Admin.CoinInventory
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IDataAccess _dataAccess;
+
+        public IndexModel(IDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public List<CoinModel> CoinInventory { get; set; }
+        public Dictionary<string, int> CoinsQuantity { get; set; }
+
+        public void OnGet()
+        {
+            CoinInventory = _dataAccess.CoinInventoryGetAll();
+
+            if (CoinInventory != null)
+            {
+                CoinsQuantity = new Dictionary<string, int>();
+
+                foreach (CoinModel coin in CoinInventory)
+                {
+                    if (CoinsQuantity.ContainsKey(coin.Name))
+                    {
+                        CoinsQuantity[coin.Name]++;
+                    }
+                    else
+                    {
+                        CoinsQuantity[coin.Name] = 1;
+                    }
+                }
+            }
+        }
+    }
+}
