7d45191d50cfc09635f6db47e2a5666972830e9b
josephttran <42635012+josephttran@users.noreply.github.com>
2020-05-07 04:02:57 -0700

    Allow only user with admin role to access coin inventory page

--- csharp-soda-machine-challenge/SodaMachine/SodaMachineRazorPages/Pages/Admin/CoinInventory/Index.cshtml.cs
+++ csharp-soda-machine-challenge/SodaMachine/SodaMachineRazorPages/Pages/Admin/CoinInventory/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SodaMachineLibrary.DataAccess;
@@ -9,6 +10,7 @@ using SodaMachineLibrary.Models;
 
 namespace SodaMachineRazorPages.Pages.Admin.CoinInventory
 {
+    [Authorize(Roles = "Admin")]
     public class IndexModel : PageModel
     {
         private readonly IDataAccess _dataAccess;
