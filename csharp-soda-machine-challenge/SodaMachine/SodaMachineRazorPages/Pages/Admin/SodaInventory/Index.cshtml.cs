9fa5613718955ba195546c3e7dc87583286f3e59
josephttran <42635012+josephttran@users.noreply.github.com>
2020-05-09 00:19:42 -0700

    Implement add to soda inventory for soda inventory page

--- csharp-soda-machine-challenge/SodaMachine/SodaMachineRazorPages/Pages/Admin/SodaInventory/Index.cshtml.cs
+++ csharp-soda-machine-challenge/SodaMachine/SodaMachineRazorPages/Pages/Admin/SodaInventory/Index.cshtml.cs
@@ -22,9 +22,10 @@ namespace SodaMachineRazorPages.Pages.Admin.SodaInventory
         }
 
         public string Message { get; set; }
+        [BindProperty]
         public string SelectedSoda { get; set; }
+        [BindProperty]
         public string SelectedSodaQuantity { get; set; }
-        public SelectList Sodas { get; set; }
         public SelectList SodaAddList { get; set; }
         public Dictionary<string, int> SodaQuantity { get; set; }
 
@@ -48,6 +49,8 @@ namespace SodaMachineRazorPages.Pages.Admin.SodaInventory
                 }
             }
 
+            SodaAddList = new SelectList(SodaQuantity, "Key", "Key");
+
             return Page();
         }
 
@@ -56,6 +59,43 @@ namespace SodaMachineRazorPages.Pages.Admin.SodaInventory
             if (!ModelState.IsValid)
             {
                 return Page();
+            }
+
+            if (string.IsNullOrEmpty(SelectedSoda))
+            {
+                Message = "Please select soda with slot";
+            }
+            else
+            {
+                if (int.TryParse(SelectedSodaQuantity, out int quantity))
+                {
+                    if (quantity > 0 && quantity < 10)
+                    {
+                        List<SodaModel> sodas = new List<SodaModel>();
+                        string[] sodaNameSlot = SelectedSoda.Split(" ");
+
+                        for (int i = 0; i < quantity; i++)
+                        {
+                            SodaModel sodaModel = new SodaModel
+                            {
+                                Name = sodaNameSlot[0],
+                                SlotOccupied = sodaNameSlot[1]
+                            };
+
+                            sodas.Add(sodaModel);
+                        }
+
+                        _dataAccess.SodaInventoryAddSodas(sodas);
+                    }
+                    else
+                    {
+                        Message = "Quantity must be greater than 0 and less than 10";
+                    }
+                }
+                else
+                {
+                    Message = "Please enter a number for quantity";
+                }
             }
 
             return OnGet();
